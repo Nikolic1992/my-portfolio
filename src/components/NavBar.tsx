576fb9c622ecfe5bdd4556e930f8cea30419e884
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-06-15 11:19:58 +1000

    feat: add responsive styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 function NavBar() {
   return (
-    <nav className="absolute top-10 left-1/2 -translate-x-1/2 flex items-center justify-center rounded-3xl border border-white/20 bg-white/10 backdrop-blur-md shadow-lg px-8 py-6">
-      <div className="flex items-center gap-8 text-mainGray">
+    <nav className="absolute top-10 left-1/2 -translate-x-1/2 flex items-center justify-center rounded-xl border border-white/20 backdrop-blur-md shadow-lg px-8 py-4 transition-colors duration-300 hover:bg-white/15  bg-[linear-gradient(90deg,rgba(22,26,49,0.6)_0%,rgba(6,9,31,0.6)_100%)]">
+      <div className="flex items-center gap-4 text-mainGray">
         <button className="nav-bar-btn">
           <h4>About</h4>
         </button>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,7 +2,7 @@ import NavBar from "../components/NavBar";
 
 function HomePage() {
   return (
-    <div className="relative flex justify-center items-center h-screen bg-[#000319] bg-[url('/grid-pattern.png')] bg-no-repeat bg-cover bg-center">
+    <div className="relative flex justify-center items-center h-screen bg-[url('/grid-pattern.png')] bg-no-repeat bg-cover bg-center px-2">
       {/* Spotlight left side */}
       <img
         src="/spotlight-left.png"
@@ -17,7 +17,7 @@ function HomePage() {
         className="absolute top-0 right-0"
       />
       <NavBar />
-      <div className="flex flex-col items-center justify-center gap-8 max-w-[1000px]">
+      <div className="flex flex-col items-center justify-center gap-8 md:gap-10 w-full md:max-w-[1000px]">
         <h3 className="text-secondaryGray text-center tracking-widest">
           Creating Dynamic Web Magic
         </h3>
@@ -25,10 +25,10 @@ function HomePage() {
           Transforming Concepts into Seamless{" "}
           <span className="text-lightPurple">User Experiences</span>
         </h1>
-        <h3 className="text-secondaryGray text-center tracking-widest">
+        <h3 className="text-secondaryGray text-center md:tracking-widest">
           Hi, I am Stevan, Fullstack Developer from Sydney
         </h3>
-        <div className="flex items-center justify-center rounded-3xl border border-white/20 bg-white/10 backdrop-blur-md shadow-lg px-8 py-6 transition-colors duration-300 hover:bg-white/15">
+        <div className="flex items-center justify-center w-full md:w-auto rounded-lg border border-white/20 shadow-lg transition-colors duration-300 hover:bg-white/15 px-8 py-6 bg-[linear-gradient(90deg,rgba(22,26,49,0.6)_0%,rgba(6,9,31,0.6)_100%)]">
           <button className="flex items-center justify-center cursor-pointer gap-4 group">
             <h4 className="text-mainGray transition-colors duration-300 group-hover:text-mainWhite">
               Checkout My Work
