4fd99f9510546a514f044c646724962fe9980c91
Stevan Nikolic <stevannikolic1992@yahoo.com>
2025-06-26 18:53:31 +1000

    update z index

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -7,7 +7,7 @@ function NavBar() {
   };
 
   return (
-    <nav className="fixed top-6 left-1/2 z-10 flex -translate-x-1/2 items-center justify-center rounded-xl border border-white/20 bg-[linear-gradient(90deg,rgba(22,26,49,0.6)_0%,rgba(6,9,31,0.6)_100%)] px-8 py-4 shadow-lg backdrop-blur-md transition-colors duration-300 hover:bg-white/15 sm:top-8 md:top-10 md:px-10">
+    <nav className="fixed top-6 left-1/2 z-100 flex -translate-x-1/2 items-center justify-center rounded-xl border border-white/20 bg-[linear-gradient(90deg,rgba(22,26,49,0.6)_0%,rgba(6,9,31,0.6)_100%)] px-8 py-4 shadow-lg backdrop-blur-md transition-colors duration-300 hover:bg-white/15 sm:top-8 md:top-10 md:px-10">
       <div className="text-mainGray flex items-center gap-4">
         <button className="nav-bar-btn" onClick={() => scrollTo("about")}>
           <h4>About</h4>
